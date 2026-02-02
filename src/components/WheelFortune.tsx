49a8ca2f61665cd151b404fa2eea6a14632c58cc
desmosdesmos <57991092+desmosdesmos@users.noreply.github.com>
2026-02-02 22:20:08 +0400

    fix: remove unused useEffect import from WheelFortune.tsx

--- src/components/WheelFortune.tsx
+++ src/components/WheelFortune.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { WheelSpinResult } from '../types/wheel';
 import { wheelPrizes, prizeProbabilities } from '../data/wheelConfig';
 import { hapticFeedback, getTelegramUser } from '../utils/telegram';
