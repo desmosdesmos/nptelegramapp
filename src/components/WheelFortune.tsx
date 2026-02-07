4c1e1f63f54c027924a29c9360a2c1e1c7608deb
desmosdesmos <57991092+desmosdesmos@users.noreply.github.com>
2026-02-07 17:20:34 +0400

    Fix build error - add missing useEffect import

--- src/components/WheelFortune.tsx
+++ src/components/WheelFortune.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { WheelSpinResult } from '../types/wheel';
 import { wheelPrizes, prizeProbabilities } from '../data/wheelConfig';
 import { hapticFeedback } from '../utils/telegram';
